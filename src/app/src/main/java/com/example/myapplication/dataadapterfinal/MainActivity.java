a9c41c1317b85ed5d525129d3c41de05ac22e505
Nadine Cumberbatch <nadine.cumberbatch@bts.tech>
2020-04-13 15:59:11 +0200

    margins adjusted

--- src/app/src/main/java/com/example/myapplication/dataadapterfinal/MainActivity.java
+++ src/app/src/main/java/com/example/myapplication/dataadapterfinal/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.myapplication.dataadapterfinal;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Object String;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] name = new String [10];
+        name [0] = "Nadine";
+        name [1] = "Emiliano";
+        name [2] = "Kanya";
+        name [3] = "Lourenco";
+        name [4] = "Paul";
+        name [5] = "Rainer";
+        name [6] = "Peter";
+        name [7] = "Danilo";
+        name [8] = "Felipe";
+        name [9] = "Rodrigo";
+
+        ArrayAdapter <String> studentsAdapter =
+                new ArrayAdapter<String>( this, android.R.layout.simple_list_item_1, name);
+
+        ListView classListView = (ListView) findViewById(R.id.studentList);
+
+        classListView.setAdapter(studentsAdapter);
+
+
+
     }
 }
